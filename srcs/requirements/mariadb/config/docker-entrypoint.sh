a226383543015e6570d45c803ca3198d905b60d2
jcummins <jcummins42prague@gmail.com>
2024-11-13 13:59:44 +0100

    FINALLY mariadb setup seems to be working. Now need to get wordpress and nginx working

--- srcs/requirements/mariadb/config/docker-entrypoint.sh
+++ srcs/requirements/mariadb/config/docker-entrypoint.sh
@@ -1,53 +1,58 @@
 #!/bin/bash
 
-# This causes script to exit immediately if any command fails, to prevent issues
+# This causes script to exit immediately if any command fails
 set -e
 
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-
-echo "Testing whether ${MYSQL_DATABASE} already exists"
-if [ ! -d "/var/lib/mysql/inception_db" ]; then
+echo "Searching for directory at /var/lib/mysql/mysql..."
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo "System tables not found at /var/lib/mysql/mysql"
 
 	echo "Initializing database..."
-    mariadb-install-db --user=mysql \
-		--datadir=/var/lib/mysql/
+	mariadb-install-db --user=mysql \
+		--datadir=/var/lib/mysql \
+		--basedir=/usr
+else
+	echo "> System table found, skipping mariadb-install-db"
+fi
 
-    echo "Starting MariaDB temporarily..."
-    mysqld --skip-networking &
-    pid="$!"
+echo "Searching for directory at /var/lib/mysql/${MYSQL_DATABASE}..."
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+	echo "> Starting MariaDB temporarily to set up database and user..."
+	mysqld --skip-networking &
+	pid="$!"
 
-    # Wait for MariaDB to be ready
-    for i in {30..0}; do
-        if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
-            break
-        fi
-        echo 'MariaDB is initializing...'
-        sleep 1
-    done
+	echo "> Waiting for MariaDB to start"
+	for i in {30..0}; do
+		if mysqladmin ping &> /dev/null; then
+			echo "> MariaDB is running";
+			break
+		fi
+		echo "> waiting..."
+		sleep 1
+	done
 
 	if [ "$i" = 0 ]; then
-		echo "Error:MAriaDB failed to start"
+		echo "> Error: MariaDB failed to start"
 		exit 1
 	fi
 
-    # Set root password and enforce password authentication
-	# heredoc EOSQL is sensitive to leading tabs unless I use this <<-
-	mysql -uroot <<-EOSQL
+	# Set root password and enforce password authentication
+	mysql -uroot <<-EOF
 		SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
 		CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 		CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 		GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 		FLUSH PRIVILEGES;
-	EOSQL
+	EOF
 
 	# Shutdown temporary MariaDB instance
 	if ! kill -s TERM "$pid" || ! wait "$pid"; then
-		echo 'MariaDB initialization process failed.'
+		echo '> MariaDB initialization process failed.'
 		exit 1
 	fi
 else
-	echo "Database ${MYSQL_DATABASE} already exists. Skipping initialization"
+	echo "> ${MYSQL_DATABASE} found, skipping initialization"
 fi
 
-# Start MariaDB in the foreground
+echo "Starting MariaDB in the foreground..."
 exec mysqld --user=mysql
